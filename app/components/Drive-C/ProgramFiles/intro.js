5f695e7811b0114855b6478030283e336c26edec
deaaprizal <dea.afrizal@icloud.com>
2021-11-30 22:50:36 +0700

    a

---
+++ app/components/Drive-C/ProgramFiles/intro.js
@@ -0,0 +1,11 @@
+import React from "react";
+export const scope = "app.component.home.program-file]";
+
+export default function Intro() {
+  return (
+      <div className='card' style={{ padding: 15 }}>
+          <h1 className="content-title">Cara Cepat Jadi Programmer 🐱‍👤</h1>
+          <p className="lead">lorem ipsum dolor sit amet, con, sed do eiusmod tempor incididunt ut labore et dol lorem ipsum dolor. Lorem ipsum dolor sit lorem ipsum dolor lorem ipsum dolor. Lorem ipsum dolor, lorem ipsum dolor sit lorem ipsum dolor</p>
+    </div>
+  );
+}
