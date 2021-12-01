f98391500537ca4395ee6dd3661827edd958297b
Tedi Suryadi <tedi.suryadi77@gmail.com>
2021-12-01 18:04:27 +0700

    dinamis theme editor, run code dan clean code

--- app/components/Drive-D/Playground/index.js
+++ app/components/Drive-D/Playground/index.js
@@ -1,41 +1,66 @@
 import React, { useState, useEffect } from "react";
+import { FaPlay } from "react-icons/fa"
+import { MdDelete } from "react-icons/md"
 import Editor from "./editor"
 export const scope = "app.containers.playground";
 
 export default function Playground() {
-  const [bgTheme, setBgTheme] = useState("dark-mode");
-  const [html, setHtml] = useState('')
-  const [srcDoc, setSrcDoc] = useState('')
+  const [css, setCss] = useState("material");
+  const [html, setHtml] = useState('');
+  const [srcDoc, setSrcDoc] = useState('');
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setSrcDoc(html)
-    }, 250)
+  const ListCss = ["material","dracula","moxer","twilight","abbott"]
 
-    return () => clearTimeout(timeout)
-  }, [html])
+  const RunCode = () => {
+    setSrcDoc(html)
+  }
 
+  const ClearCode = () => {
+    setSrcDoc("")
+    setHtml("")
+  }
 
   return (
-    <div className={`content-wrapper ${bgTheme}`}>
+    <div className={`content-wrapper sidebar`}>
       <div className="content row">
-        <div className="col-12-xl col-lg">
-          <Editor
-            language="xml"
-            displayName="HTML"
-            value={html}
-            onChange={setHtml}
-          />
-          <iframe
-            sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
-            style={{ backgroundColor: "#ffffff" }}
-            srcDoc={srcDoc}
-            title="output"
-            sandbox="allow-scripts"
-            frameBorder="0"
-            width="100%"
-            height="100%"
-          />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-6-xl col-lg">
+              <div className="btn-group" role="group" aria-label="Basic example">
+                <button className="btn" type="button" onClick={ () => RunCode() }><FaPlay size="18"/></button>
+                <button className="btn" type="button" onClick={ () => ClearCode() }><MdDelete size="20"/></button>
+                <select className="form-control" onChange={ (e) => setCss(e.target.value) }>
+                  { ListCss.map( (css,i) => {
+                      return (
+                        <option value={css} key={i}>{css}</option>
+                      )
+                    }) 
+                  }
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="row" >
+            <div className="col-6-xs col-lg">
+              <Editor
+                language="xml"
+                value={html}
+                theme={css}
+                onChange={setHtml}
+              />
+            </div>
+            <div className="col-6-xs col-lg">
+              <iframe
+                sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+                style={{ backgroundColor: "#808080" }}
+                srcDoc={srcDoc}
+                title="output"
+                frameBorder="0"
+                width="100%"
+                height="100%"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
